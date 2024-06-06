8760672c50c77029d03d38455c57644e1dffb8eb
Gustavo Brito <gustavobritoland@gmail.com>
2024-06-06 19:43:08 -0300

    Criando novos testes

--- soma.test.js
+++ soma.test.js
@@ -1,5 +1,11 @@
 const soma = require('./soma')
 
-test('verificar soma', () => {
-    expect(soma(3, 8)).toBe(11)
+describe('Teste soma', () => {
+    test('verificar soma', () => {
+        expect(soma(3, 8)).toBe(11)
+    })
+
+    test('Verifica soma incorreta', () => {
+        expect(soma(3, 4)).not.toBe(13)
+    })
 })
